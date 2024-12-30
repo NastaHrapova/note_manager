1ecdb8a322553f5b1498e54bf09caa33ed5715d1
NastaHrapova <nhrapova99@gmail.com>
2024-12-31 00:50:47 +0300

    этап 1 задание 1

---
+++ greetings.py
@@ -0,0 +1,17 @@
+user_name='Анастасия'
+print(user_name)
+
+title = "Планы"
+print(title)
+
+content="сходить в магазин, узнать о фильме"
+print(content)
+
+status="Завершина"
+print(status)
+
+created_data='20.03.2025'
+print(created_data)
+
+issue_date='20.04.2025'
+print(issue_date)
