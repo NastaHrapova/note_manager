ceb72d400441dd383a6ad6566b11ebba30fce7ba
NastaHrapova <nhrapova99@gmail.com>
2025-01-03 13:55:17 +0300

    Add files via upload
    этап 1 задание 3

---
+++ add_input.py
@@ -0,0 +1,8 @@
+title = input('Введите заголовок заметки: ')
+text = input('Введите описание заметки: ')
+number = int(input('Введите число написания заметки: '))
+month = int(input('Укажите месяц (числом): '))
+year = int(input('Укажите год: '))
+
+print(title)
+print(text)
